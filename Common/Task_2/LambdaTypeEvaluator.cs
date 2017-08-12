d39fc0b27a760a467442945663129e77ae6df7bb
Vladimir <vvkraf@gmail.com>
2017-08-12 13:56:44 +0300

    Fix output for 2nd task.

--- Common/Task_2/LambdaTypeEvaluator.cs
+++ Common/Task_2/LambdaTypeEvaluator.cs
@@ -9,122 +9,128 @@ using Task_2.LambdaType;
 
 namespace Task_2
 {
-    public class LambdaTypeEvaluator
+  public class LambdaTypeEvaluator
+  {
+    private LambdaExpression lambda;
+    private Dictionary<Variable, IType> Context = new Dictionary<Variable, IType>();
+    public LambdaTypeEvaluator(LambdaExpression lambda)
     {
-        private LambdaExpression lambda;
-        private Dictionary<Variable, IType> Context = new Dictionary<Variable, IType>();
-        public LambdaTypeEvaluator(LambdaExpression lambda)
-        {
-            this.lambda = lambda;
-        }
-
-        public IType GetLambdaType()
-        {
-            HashSet<Equation> equations;
-            var variableType = new Dictionary<Variable, IType>();
-            IType type = CalculateType(lambda, out equations, variableType);
-
-            var result = new Unificator(equations).Solve();
-
-
-            foreach (var var in variableType.Keys)
-            {
-                Context[var] = Subst(variableType[var], result);
-            }
-            return Subst(type, result);
-        }
-    
-        public static IType Subst(IType expr, Dictionary<SingleType, IType>  map)
-        {
-            if (expr is SingleType)
-            {
-                var singleType = expr as SingleType;
-                if (map.ContainsKey(singleType))
-                  return map[singleType];
-                return expr;
-            } else if (expr is Implication)
-            {
-                var impl = expr as Implication;
-                var left = Subst(impl.Left, map);
-                var right = Subst(impl.Right, map);
-                if (left != impl.Left || right != impl.Right)
-                {
-                    return new Implication(left, right);
-                }
-                return impl;
-            } else if (expr is Universal)
-            {
-	              var universal = expr as Universal;
-								if (map.ContainsKey(universal.Variable))
-								{
-										return universal;
-								}
-								else
-								{
-										var e = Subst(universal.Expression, map);
-										if (e == universal.Expression)
-										{
-											return expr;
-										}
-										return new Universal(universal.Variable, e);
-								}
-            } else
-            {
-                throw new Exception("Unsupported type");
-            }
-        }
-
-        public void PrintContext(StreamWriter sw)
-        {
-            foreach (var kvp in Context)
-            {
-                sw.WriteLine(kvp.Key + " : " + kvp.Value);
-            }
-        }
-
-        private IType CalculateType(LambdaExpression lambda, out HashSet<Equation> equations, Dictionary<Variable, IType> type)
-        {
-            if (lambda is Abstraction)
-            {
-                var abstraction = lambda as Abstraction;
-                IType prev = null;
-
-                var variableType = type[abstraction.Variable] = new SingleType("'t" + abstraction.Variable.Name);
-                var exprType = CalculateType(abstraction.Expression, out equations, type);
-
-                return new Implication(variableType, exprType);
-               
-            } else if (lambda is Application)
-            {
-                var application = lambda as Application;
-                HashSet<Equation> left;
-                HashSet<Equation> right;
-
-                var leftType = CalculateType(application.Left, out left, type);
-                var rightType = CalculateType(application.Right, out right, type);
-
-                equations = new HashSet<Equation>(left);
-                foreach (var e in right) equations.Add(e);
-                var newType = new SingleType(NextName());
-                equations.Add(new Equation(leftType, new Implication(rightType, newType)));
-
-                return newType;
-
-            }
-            else if (lambda is Variable)
-            {
-                var variable = lambda as Variable;
-                equations = new HashSet<Equation>();
-                type[variable] = new SingleType("'t" + variable.Name);
-                return type[variable];
-            }
-            throw new Exception("Unsupported type");
-        }
-
-        int name = 0;
-        private string NextName()
-        {
-            return "'t" + (name++);
-        }
+      this.lambda = lambda;
     }
+
+    public IType GetLambdaType()
+    {
+      HashSet<Equation> equations;
+      var variableType = new Dictionary<Variable, IType>();
+      IType type = CalculateType(lambda, out equations, variableType);
+
+      var result = new Unificator(equations).Solve();
+
+
+      foreach (var var in variableType.Keys)
+      {
+        Context[var] = Subst(variableType[var], result);
+      }
+      return Subst(type, result);
+    }
+
+    public static IType Subst(IType expr, Dictionary<SingleType, IType> map)
+    {
+      if (expr is SingleType)
+      {
+        var singleType = expr as SingleType;
+        if (map.ContainsKey(singleType))
+          return map[singleType];
+        return expr;
+      }
+      else if (expr is Implication)
+      {
+        var impl = expr as Implication;
+        var left = Subst(impl.Left, map);
+        var right = Subst(impl.Right, map);
+        if (left != impl.Left || right != impl.Right)
+        {
+          return new Implication(left, right);
+        }
+        return impl;
+      }
+      else if (expr is Universal)
+      {
+        var universal = expr as Universal;
+        if (map.ContainsKey(universal.Variable))
+        {
+          return universal;
+        }
+        else
+        {
+          var e = Subst(universal.Expression, map);
+          if (e == universal.Expression)
+          {
+            return expr;
+          }
+          return new Universal(universal.Variable, e);
+        }
+      }
+      else
+      {
+        throw new Exception("Unsupported type");
+      }
+    }
+
+    public void PrintContext(StreamWriter sw)
+    {
+      foreach (var kvp in Context)
+      {
+        sw.WriteLine(kvp.Key + " : " + kvp.Value);
+      }
+    }
+
+    private IType CalculateType(LambdaExpression lambda, out HashSet<Equation> equations, Dictionary<Variable, IType> type)
+    {
+      if (lambda is Abstraction)
+      {
+        var abstraction = lambda as Abstraction;
+        IType prev = null;
+
+        var variableType = type[abstraction.Variable] = new SingleType("'t" + abstraction.Variable.Name);
+        var exprType = CalculateType(abstraction.Expression, out equations, type);
+
+        type.Remove(abstraction.Variable);
+
+        return new Implication(variableType, exprType);
+
+      }
+      else if (lambda is Application)
+      {
+        var application = lambda as Application;
+        HashSet<Equation> left;
+        HashSet<Equation> right;
+
+        var leftType = CalculateType(application.Left, out left, type);
+        var rightType = CalculateType(application.Right, out right, type);
+
+        equations = new HashSet<Equation>(left);
+        foreach (var e in right) equations.Add(e);
+        var newType = new SingleType(NextName());
+        equations.Add(new Equation(leftType, new Implication(rightType, newType)));
+
+        return newType;
+
+      }
+      else if (lambda is Variable)
+      {
+        var variable = lambda as Variable;
+        equations = new HashSet<Equation>();
+        type[variable] = new SingleType("'t" + variable.Name);
+        return type[variable];
+      }
+      throw new Exception("Unsupported type");
+    }
+
+    int name = 0;
+    private string NextName()
+    {
+      return "'t" + (name++);
+    }
+  }
 }
